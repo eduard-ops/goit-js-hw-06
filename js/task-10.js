185141a66e881bc95d6ea0bd80fa3285da3386ed
Edward14887 <epustynnik@gmail.com>
2022-01-20 01:10:14 +0200

    js

--- js/task-10.js
+++ js/task-10.js
@@ -1,1 +1,35 @@
+const inputEl = document.querySelector("#controls input");
 
+const buttonCreat = document.querySelector("button[data-create]");
+
+const buttonDestroy = document.querySelector("button[data-destroy]");
+
+const boxesDiv = document.querySelector("#boxes");
+
+buttonCreat.addEventListener("click", createDivMarkup);
+
+function createDivMarkup(evt) {
+  evt.preventDefault();
+
+  let divWidth = 30;
+  let divHeight = 30;
+
+  for (let i = 0; i < inputEl.value; i += 1) {
+    const newDivEl = document.createElement("div");
+    newDivEl.style.backgroundColor = `${getRandomHexColor()}`;
+    newDivEl.style.width = `${(divWidth += 10)}px`;
+    newDivEl.style.height = `${(divHeight += 10)}px`;
+    boxesDiv.append(newDivEl);
+  }
+}
+
+buttonDestroy.addEventListener("click", onClickDestroyButton);
+
+function onClickDestroyButton() {
+  boxesDiv.innerHTML = "";
+  inputEl.value = "";
+}
+
+function getRandomHexColor() {
+  return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
+}
